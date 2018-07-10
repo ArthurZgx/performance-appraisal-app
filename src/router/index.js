a9d5e39b9216e0e7369ad68bc73957ffa92d4d9d
964269669 <964269669@qq.com>
2018-07-10 17:54:54 +0800

    将路由文件单独提出

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Serve from '@/components/Serve'
+import Complete from '@/components/Complete'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Serve',
+      name: 'Serve',
+      component: Serve
+    },
+    {
+      path: '/Complete',
+      name: 'Complete',
+      component: Complete
     }
   ]
 })
