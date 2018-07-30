87fb8555165fe84b2c377b2d7f3ec6738caebdfa
golylord <38677202+golylord@users.noreply.github.com>
2018-07-30 11:59:45 +0800

    更新多选按钮样式

--- src/components/serve/Serve.vue
+++ src/components/serve/Serve.vue
@@ -18,16 +18,17 @@
             ref="search">
     </search>
     <group class="home_group groupList">
-      <div v-for="(item,index) in serveList" :key="index">
+      <div class="aGroupList" v-for="(item,index) in serveList" :key="index">
         <div class="serveClassifyTitle">{{item.title}}</div>
         <div>
            <div v-for="(list,index) in item.list" :key="index" class="aListData">
             <div class="listCheck"  @click="listCheckClick(list)">
-              <check-icon :value.sync="list.checked"></check-icon>
+              <!-- <check-icon :value.sync="list.checked"></check-icon> -->
+              <span @click="list.checked=!list.checked" :class="list.checked?'my-check-icon-clicked':'my-check-icon'"></span>
             </div>
             <div class="listInfo" @click="goToServeComment(list)">
-                <div>{{list.name}}</div>
-                <div>{{list.time}}</div>
+                <div class="listInfoName">{{list.name}}</div>
+                <div class="listInfoTime">{{list.time}}</div>
             </div>
            </div>
         </div>
@@ -41,19 +42,35 @@
       <flexbox style="background:white;">
         <flexbox-item :span="3.5">
           <div @click="clickAll" class="home_div">
-            <check-icon :value.sync="checkedAll"></check-icon>全选
-          </div>
+            <!-- <check-icon :value.sync="checkedAll"></check-icon>全选 -->
+              <span @click="checkedAll = !checkedAll" :class="checkedAll?'my-check-icon-clicked':'my-check-icon'"></span>全选          
+          </div> 
         </flexbox-item>
         <flexbox-item>
-          <x-button type="primary">一键提交</x-button>
+          <x-button style="background:#3891f0;color:white;" @click.native="showSubmitDialog = true">一键提交</x-button>
         </flexbox-item>
       </flexbox>
     </sticky>
+    <!-- 弹出框 -->
+    <div>
+      <x-dialog v-model="showSubmitDialog" class="dialog-demo">
+        <span class="img-box">
+          <img src="http://ui.haomo-tech.com/%E9%93%B6%E4%BF%A1%E9%95%BF%E8%BF%9C/assets/Path%20.png" alt="">
+        </span>
+        <div class="box-title">
+           确认使用一键提交说所有人的评价吗</div>
+        <div class="bottomBtn" @click="showSubmitDialog=false">
+          <span class="vux-cancel">取消</span>
+          <span class="vux-sure">确认</span>
+        </div>
+      </x-dialog>
+    </div>
+
   </div>
 </template>
 
 <script>
-import { Group, Cell, Tabbar, TabbarItem, XHeader, Icon, Search, CheckIcon, XButton, Flexbox, FlexboxItem, Sticky } from 'vux'
+import { Group, Cell, Tabbar, TabbarItem, XHeader, Icon, Search, CheckIcon, XButton, Flexbox, FlexboxItem, Sticky, XDialog } from 'vux'
 // import _ from 'lodash'
 
 var i = 0
@@ -73,7 +90,8 @@ export default {
     XButton,
     Flexbox,
     Sticky,
-    FlexboxItem
+    FlexboxItem,
+    XDialog
   },
   data() {
     return {
@@ -83,6 +101,7 @@ export default {
       commonList: ['name1', 'name2', 'name3', 'name4'], // 所有供选择的列表
       checkedAll: false, // 是否全选
       childNodeNum: 0,
+      showSubmitDialog: false,
       serveList: [
         {
           title: '技术部',
@@ -205,6 +224,8 @@ export default {
 .serveClassifyTitle {
   padding: 10px 5px;
   border-bottom: 1px solid #eee;
+  color: #666;
+  font-size: 14px;
 }
 .listCheck {
   height: 50px;
@@ -212,6 +233,98 @@ export default {
 }
 .listInfo {
   height: 50px;
-  margin-left: 30px;
+  margin-left: 10px;
+  line-height: 25px;
+}
+.listInfoName {
+  font-size: 16px;
+  color: #333;
+}
+.listInfoTime {
+  font-size: 13px;
+  color: #888;
+}
+.aListData {
+  border-bottom: 1px solid #eee;
+}
+
+.weui-dialog {
+  border-radius: 8px;
+  padding-bottom: 8px;
+  height: 150px;
+  max-width: 400px;
+}
+.dialog-title {
+  color: #666;
+}
+.img-box {
+  overflow: hidden;
+}
+.vux-close {
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+.img-box {
+  float: left;
+  background: #e11c1c;
+  display: block;
+  width: 44px;
+  height: 44px;
+  border-radius: 50%;
+  margin-left: 20px;
+  margin-top: 20px;
+}
+.img-box img {
+  height: 50%;
+  margin-top: 11px;
+}
+.box-title {
+  line-height: 20px;
+  margin-top: 20px;
+  text-align: left;
+  margin-left: 90px;
+  margin-right: 20px;
+}
+.vux-sure {
+  color: #e11c1c;
+  margin-left: 20px;
+}
+.bottomBtn {
+  position: absolute;
+  bottom: 7px;
+  right: 10px;
+}
+.my-check-icon {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  background: white;
+  position: relative;
+  margin-left: 20px;
+  border: 1px solid #c7c7c7;
+  margin-right: 10px;
+}
+.my-check-icon-clicked {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  background: #3891f0;
+  border: 1px solid #3891f0;
+  position: relative;
+  margin-left: 20px;
+  margin-right: 10px;
+}
+.my-check-icon-clicked::after {
+  content: '\00a0';
+  display: inline-block;
+  border: 2px solid #fff;
+  border-top-width: 0;
+  border-right-width: 0;
+  width: 10px;
+  height: 5px;
+  -webkit-transform: rotate(-50deg);
+  position: absolute;
+  top: 2px;
+  left: 1px;
 }
 </style>
