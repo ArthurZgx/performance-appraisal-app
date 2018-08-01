dad771960e1123d7f162965cb96f8ed8538c988c
964269669 <964269669@qq.com>
2018-08-01 11:42:11 +0800

    request

--- src/api/login.js
+++ src/api/login.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-export function loginByUsername(username, password,token) {
+export function loginByUsername(username, password, token) {
   const data = {
     username,
     password,
--- src/components/serve/Serve.vue
+++ src/components/serve/Serve.vue
@@ -74,7 +74,7 @@
 <script>
 import { Group, Cell, Tabbar, TabbarItem, XHeader, Icon, Search, CheckIcon, XButton, Flexbox, FlexboxItem, Sticky, XDialog, Toast } from 'vux'
 // import _ from 'lodash'
-// import request from '@/utils/request'
+import request from '@/utils/request'
 // import request from '@/utils/request'
 
 var i = 0
@@ -134,7 +134,9 @@ export default {
   },
   created() {
     this.getTotalLength()
-
+    request('main_job_service_evaluations').then(res => {
+      console.log(138, res)
+    })
   },
   watch: {
     // checkedAll(newValue, oldValue) {
