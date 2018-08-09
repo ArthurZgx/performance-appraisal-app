55e998595e2cb04a52c450f34d7d2057f5d3d38c
rengensheng <rengensheng@lingdao.com>
2018-08-09 16:33:36 +0800

    提示信息

--- src/components/userJobComplete/userJobComplete.vue
+++ src/components/userJobComplete/userJobComplete.vue
@@ -17,8 +17,7 @@
               <td>{{alist.taskName}}</td>
               <td>{{alist.planContent}}</td>
             </tr>
-            <tr><td colspan="2" v-if="list.length == '0'">没有数据</td></tr>
-            {{msg}}
+            <tr><td colspan="2" v-if="list.length == '0'">暂无数据</td></tr>
           </tbody>
         </x-table>
       <!-- </group> -->
