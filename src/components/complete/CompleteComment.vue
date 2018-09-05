5fb6da85491e7b6fddff771a615041cfbc727aa6
964269669 <964269669@qq.com>
2018-09-05 17:15:16 +0800

    最终系数小数位

--- src/components/complete/CompleteComment.vue
+++ src/components/complete/CompleteComment.vue
@@ -130,10 +130,11 @@
         let temp = 0
         _.each(this.taskList, function(item, key) {
           if (item.completionRatio) {
-            temp += parseInt(item.completionRatio) * parseInt(item.weights) / 100
+            temp += parseInt(item.completionRatio) * parseInt(item.weights)
           }
         })
-        return temp
+        temp = temp / 100
+        return temp.toFixed(1)
       }
     },
     methods: {
