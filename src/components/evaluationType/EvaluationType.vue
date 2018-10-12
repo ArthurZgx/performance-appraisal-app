ece0020e9e3db5fa8b243407eb0d75a39ebcd5e0
rengensheng <rengensheng@lingdao-tech.com>
2018-10-12 14:55:25 +0800

    改名

--- src/components/evaluationType/EvaluationType.vue
+++ src/components/evaluationType/EvaluationType.vue
@@ -7,9 +7,9 @@
                 考评记录
       </x-header>
       <group>
-      <cell title="已提交评价" is-link :link="{name:'evaluationRecord',params:{type:'alreadySubmit'}}"></cell>
-      <cell title="已过期评价" is-link :link="{name:'evaluationRecord',params:{type:'pastSubmit'}}"></cell>
-      <cell title="还在考评周期内" is-link :link="{name:'evaluationRecord',params:{type:'inSubmit'}}"></cell>
+      <cell title="已评价" is-link :link="{name:'evaluationRecord',params:{type:'alreadySubmit'}}"></cell>
+      <cell title="已过期" is-link :link="{name:'evaluationRecord',params:{type:'pastSubmit'}}"></cell>
+      <cell title="待评价" is-link :link="{name:'evaluationRecord',params:{type:'inSubmit'}}"></cell>
     </group>
   </div>
 </template>
