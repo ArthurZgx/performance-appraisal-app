b8bffb368c1620ab77e19193dd172df63d50b3c7
rengensheng <goylord2@foxmail.com>
2018-10-15 11:03:14 +0800

    服务质量时间导致报错

--- src/components/serve/Serve.vue
+++ src/components/serve/Serve.vue
@@ -319,9 +319,9 @@ export default {
       var filter = ''
       console.log(res)
       for (var i = 0, len = res.data.length; i < len; i++) {
-        if (res.data[i].includes.main_job_service_evaluation.evaluationTime === null) {
-          res.data[i].includes.main_job_service_evaluation.evaluationTime = '2018-08-02 12:02:38'
-        }
+        // if (res.data[i].includes.main_job_service_evaluation.evaluationTime === null) {
+        //   res.data[i].includes.main_job_service_evaluation.evaluationTime = '2018-08-02 12:02:38'
+        // }
         if (res.data[i].superior.praiseNumber === null) {
           res.data[i].superior.praiseNumber = res.data[i].superior.numberVotes
         }
@@ -331,7 +331,7 @@ export default {
           res.data[i].includes.main_job_service_evaluation.title = res.data[i].includes.main_job_service_evaluation.title.split('—')[0]
           tempArray.push({
             name: res.data[i].includes.main_job_service_evaluation.title.split('的')[0],
-            time: res.data[i].includes.main_job_service_evaluation.evaluationTime.split(' ')[0],
+            // time: res.data[i].includes.main_job_service_evaluation.evaluationTime.split(' ')[0],
             type: res.data[i].includes.main_job_service_evaluation.type,
             status: res.data[i].superior.status,
             id: res.data[i].superior.id,
