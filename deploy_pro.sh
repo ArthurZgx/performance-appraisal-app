c0526833dec7c1a9f4db3ddb1aa9d2f25027b70e
任根胜 <goylord2@foxmail.com>
2020-03-17 15:22:56 +0800

    fix: 更正webpack路径配置&更改部署地址

--- deploy_pro.sh
+++ deploy_pro.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 npm run build
-rsync -avz -e "ssh -p3005" dist/index.html  root@lingdao-tech.com:/data/www/html/app/
-rsync -avz -e "ssh -p3005" dist/static  root@lingdao-tech.com:/data/www/html/
+# rsync -avz -e "ssh -p3005" dist/index.html  root@lingdao-tech.com:/data/www/html/app/
+# rsync -avz -e "ssh -p3005" dist/static  root@lingdao-tech.com:/data/www/html/
+
+rsync -avz -e "ssh" dist/* ubuntu@58.87.92.223:/var/www/html/app/
+# rsync -avz -e "ssh" dist/* ubuntu@58.87.92.223:/var/www/html/
